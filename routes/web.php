73d203a0f70a16c42a88cf15e6780af5cd966801
Ian Pinto <ianpinto1980@gmail.com>
2018-06-15 15:27:12 +0530

    +Beginning Laravel Chapter 4 Routing

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,65 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello', function () {
+    echo "<html>
+
+    <head>
+        <title>Laravel</title>
+        <style>
+            html,
+            body {
+                height: 100%;
+            }
+            
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-size: 20px;
+                font-family: 'Lato';
+            }
+            
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+            
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+            
+            .title {
+                font-size: 110px;
+            }
+        </style>
+    </head>
+    
+    <body>
+        <div class='container'>
+            <div class='content'>
+                <div class='title'>Hello, Dear Reader</div>
+                <div class='container'>
+                    <div class='content'>How are you? I hope you would love this book!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+    
+    </html>";
+});
+
+Route::get( 'hello/{name}', function ($name){
+    echo "Hello " . $name;
+});
+
+Route::any('any', function (){
+    return "Anything is possible if you try hard!";
+});
+
+Route::when('*', 'csrf', ['post', 'put', 'patch']);
